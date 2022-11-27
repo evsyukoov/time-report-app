de143b53b639c7f55b9a08db8735ce42741d9b41
evsyukoov <89853197824@mail.ru>
2022-11-27 17:46:10 +0300

    Script for systemctl

---
+++ entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# вызывать в /etc/rc.local (при старте системы)
+
+server_bot_dir='/home/denis/time-report-app'
+version=$(cat ${server_bot_dir}/pom.xml | grep \<tg-bot.version\> | cut -d '    >' -f2 | cut -d '<' -f1)
+
+# поднимаем бота
+cd ${server_bot_dir}/tg-bot
+java -jar target/tg-bot-${version}.jar > /dev/null 2>&1 &
+
+# поднимаем веб-гуи
+cd ${server_bot_dir}/gui-client
+java -jar target/gui-client-${version}.jar > /dev/null 2>&1 &
+cd ${server_bot_dir}
